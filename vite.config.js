cfcdb49212ec75e957ea21f93e0c25fe9cd84eaf
raynier mora <93624251+rayniermora@users.noreply.github.com>
2025-10-16 15:45:52 -0500

    config del vite

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+<<<<<<< Updated upstream
   base: '/portafolio/'
+=======
+  base: '/portafolio/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name].[hash].[ext]'
+      }
+    }
+  },
+  publicDir: 'public'
+>>>>>>> Stashed changes
 })
